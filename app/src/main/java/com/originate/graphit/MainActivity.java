c13bb537bb568fecec569c877a3c6869f5362046
Brandon Worl <brandon.worl@originate.com>
2014-08-06 14:13:52 -0700

    Fix for crash on device boot

--- app/src/main/java/com/originate/graphit/MainActivity.java
+++ app/src/main/java/com/originate/graphit/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.originate.graphit.metrics.MetricsList;
 import com.originate.graphit.metrics.battery.BatteryModel;
 import com.originate.graphit.metrics.MetricModel;
 import com.originate.graphit.metrics.network.NetworkModel;
@@ -26,13 +27,13 @@ import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-    private ArrayList<MetricModel> metricsList = new ArrayList<MetricModel>();
+    private ArrayList<MetricModel> metricsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        populateMetricsList();
+        metricsList = new MetricsList(this).getMetricsList();
         startBackgroundService();
 
         setContentView(R.layout.activity_main);
@@ -43,15 +44,8 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    private void populateMetricsList() {
-        metricsList.add(new BatteryModel(this));
-        metricsList.add(new ScreenUsageModel(this));
-        metricsList.add(new NetworkModel(this));
-    }
-
     private void startBackgroundService() {
         Intent intent = new Intent(this, DataCollectionService.class);
-        intent.putParcelableArrayListExtra("metricsList", metricsList);
         PendingIntent pintent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
         Calendar cal = Calendar.getInstance();
         AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
